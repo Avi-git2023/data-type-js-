9698262133dcffb171781b56ab549287dec5aa74
Avi-git2023 <146064968+Avi-git2023@users.noreply.github.com>
2023-09-28 18:51:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function changea(){
+    var name="Penguine";
+    var count="10";
+    if(true){
+        document.getElementById("name").textContent=name+" : "+count;
+    
+    }
+        
+    }
+   changea();
+function changeb(){
+    const isRightImageOfHoney="true";
+    if (true){
+        document.getElementById("honey").textContent=isRightImageOfHoney;
+    }
+}
+     
+
+    
+
